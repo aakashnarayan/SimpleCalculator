13aa3c68e7e293de0f4534d7385a6e55b419c438
aakashnarayan <42978492+aakashnarayan@users.noreply.github.com>
2018-09-18 13:53:44 -0400

    Add files via upload

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,21 @@
-
+/*
+ * 
+ * @author Aakash Narayan
+ * Calculator
+ * Period 7
+ * 
+ */
 public class Calculator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public int add(int num1, int num2)
+	{
+		int ans = num1 + num2;
+		return ans;
+	}
+	public int subt(int num1, int num2)
+	{
+		int subAns = num1 - num2;
+		return subAns;
 	}
 
 }
--- src/CalculatorMain.java
+++ src/CalculatorMain.java
@@ -1,10 +1,17 @@
-
+/*
+ * 
+ * @author Aakash Narayan
+ * CalculatorMain
+ * Period 7
+ * 
+ */
 public class CalculatorMain {
 
 	public static void main(String[] args) 
 	{
 		Calculator calc = new Calculator();
-		calc.add(2, 4);
+		System.out.println(calc.add(2, 4));
+		System.out.println(calc.add(20, 30));
 	}
 
 }
